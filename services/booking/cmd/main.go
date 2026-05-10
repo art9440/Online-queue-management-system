2fcfdc74c9b5ea69ea3508bc4800e2b6f412615e
art9440 <art9440@gmail.com>
2026-05-10 17:59:15 +0700

    made main.go

--- services/booking/cmd/main.go
+++ services/booking/cmd/main.go
@@ -1,1 +1,62 @@
-package booking
+package main
+
+import (
+	"Online-queue-management-system/libs/logger"
+	"Online-queue-management-system/services/booking/config"
+	"Online-queue-management-system/services/booking/internal/infrastructure/app"
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	log := logger.New(logger.Config{
+		Level:  slog.LevelInfo,
+		JSON:   false,
+		Source: true,
+	})
+
+	slog.SetDefault(log)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	ctx = logger.With(ctx, log)
+
+	if err := run(ctx); err != nil {
+		slog.Error("something went wrong while running branches service", "err", err)
+		stop()
+		os.Exit(1)
+	}
+
+	stop()
+}
+
+func run(ctx context.Context) error {
+	log := logger.From(ctx)
+	cfg, err := config.LoadConfig(ctx)
+	if err != nil {
+		log.Error("error loading config", "err", err)
+		return err
+	}
+
+	dbCfg, err := .LoadDBConfig(ctx)
+	if err != nil {
+		log.Error("error loading db config", "err", err)
+		return err
+	}
+
+	app, err := app.NewApp(ctx, *cfg, *dbCfg)
+	if err != nil {
+		log.Error("error creating branches app", "err", err)
+		return err
+	}
+
+	if err := app.Run(ctx); err != nil {
+		log.Error("error starting branches service", "err", err)
+		return err
+	}
+
+	return nil
+}
