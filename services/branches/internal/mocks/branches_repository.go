afe79a2eff64d5ed8002c356d74fd4a6aa22dbac
art9440 <art9440@gmail.com>
2026-05-18 20:30:02 +0700

    fixed mock repo

--- services/branches/internal/mocks/branches_repository.go
+++ services/branches/internal/mocks/branches_repository.go
@@ -6,40 +6,72 @@ import (
 )
 
 type BranchesRepository struct {
-	ByBusinessID    map[int64][]domain.Branch
-	ByID            map[int64][]domain.Branch
-	LastBusinessID  int64
-	LastBranchID    int64
+	ByBusinessID        map[int64][]domain.Branch
+	ByID                map[int64][]domain.Branch
+	EmployeesByBranchID map[int64][]domain.Employee
+
+	LastBusinessID int64
+	LastBranchID   int64
+
+	LastEmployeesBranchID int64
+
 	BusinessIDCalls int
 	IDCalls         int
-	Err             error
+	EmployeesCalls  int
+
+	Err error
 }
 
 func NewBranchesRepository() *BranchesRepository {
 	return &BranchesRepository{
-		ByBusinessID: make(map[int64][]domain.Branch),
-		ByID:         make(map[int64][]domain.Branch),
+		ByBusinessID:        make(map[int64][]domain.Branch),
+		ByID:                make(map[int64][]domain.Branch),
+		EmployeesByBranchID: make(map[int64][]domain.Employee),
 	}
 }
 
 func (r *BranchesRepository) GetByBusinessID(_ context.Context, businessID int64) ([]domain.Branch, error) {
 	r.BusinessIDCalls++
 	r.LastBusinessID = businessID
+
 	if r.Err != nil {
 		return nil, r.Err
 	}
+
 	return r.ByBusinessID[businessID], nil
 }
 
 func (r *BranchesRepository) GetByID(_ context.Context, branchID int64) ([]domain.Branch, error) {
 	r.IDCalls++
 	r.LastBranchID = branchID
+
 	if r.Err != nil {
 		return nil, r.Err
 	}
+
 	branches, ok := r.ByID[branchID]
 	if !ok {
 		return nil, domain.ErrBranchNotFound
 	}
+
 	return branches, nil
+}
+
+func (r *BranchesRepository) GetEmployeesByBranchID(
+	_ context.Context,
+	branchID int64,
+) ([]domain.Employee, error) {
+	r.EmployeesCalls++
+	r.LastEmployeesBranchID = branchID
+
+	if r.Err != nil {
+		return nil, r.Err
+	}
+
+	employees, ok := r.EmployeesByBranchID[branchID]
+	if !ok {
+		return []domain.Employee{}, nil
+	}
+
+	return employees, nil
 }
