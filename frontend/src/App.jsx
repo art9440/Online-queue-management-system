e99033b1cdd23c5951f4ecf0d960829d7f05ad4d
Navsika <ermolaevauliana05@yandex.ru>
2026-05-26 13:26:56 +0800

    add path to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegistrationPage/>}/>
         <Route path="/verify" element={<VerifyPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/public/:registrationSlug" element={<PublicBookingPage />} />
       </Routes>
     </Router>
